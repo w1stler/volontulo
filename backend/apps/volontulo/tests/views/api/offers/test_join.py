07bcc6225c870990a01348b903657f7ae7a80baa
w1stler <wojciech.patelka@gmail.com>
2018-08-22 20:15:03 +0200

    [981] Test fixes

--- backend/apps/volontulo/tests/views/api/offers/test_join.py
+++ backend/apps/volontulo/tests/views/api/offers/test_join.py
@@ -20,16 +20,24 @@ class TestAuthenticatedUserJoinOffer(TestOffersCommons, APITestCase):
         self.client.login(username='admin@example.com', password='123admin')
 
     def test_user_join_offer_authenticated(self):
-        """Test offer's delete status for admin user."""
+        """Test offer's created status for admin user."""
         response = self.client.post('/api/offers/1/join/')
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+    def test_user_join_not_existing_offer(self):
+        """Test offer's 404 status for non existing offer."""
+        response = self.client.post('/api/offers/1999999999999/join/')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
 
 class TestNotAuthenticatedUserJoinOffer(TestOffersCommons, APITestCase):
 
+    """Tests for REST API's join offer view for not authenitcated user"""
+
     def test_user_join_offer_not_authenticated(self):
-        """Test offer's delete status for not logged in user."""
+        """Test offer's forbidden status for not logged in user."""
         response = self.client.post('/api/offers/1/join/')
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
