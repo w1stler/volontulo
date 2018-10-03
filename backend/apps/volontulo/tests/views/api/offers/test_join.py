c63c57c74a88c17f3bd2d0c208e393697a6839e4
w1stler <wojciech.patelka@gmail.com>
2018-10-03 21:08:42 +0200

    [981] Cleanup

--- backend/apps/volontulo/tests/views/api/offers/test_join.py
+++ backend/apps/volontulo/tests/views/api/offers/test_join.py
@@ -12,31 +12,25 @@ from apps.volontulo.factories import UserFactory
 from apps.volontulo.factories import OfferFactory
 
 
-class TestAuthenticatedUserJoinOffer(TestOffersCommons, APITestCase):
+class TestAuthenticatedUserJoinOffer(APITestCase):
 
     """Tests for REST API's join offer view for authenticated user."""
-    @classmethod
-    def setUpClass(cls):
-        cls.user = UserFactory.create()
-        cls.offer = OfferFactory.create()
-        cls.offer.publish()
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.user.delete()
-        cls.offer.organization.delete()
-        cls.offer.delete()
 
     def setUp(self):
         """Set up each test."""
         super(TestAuthenticatedUserJoinOffer, self).setUp()
+        self.user = UserFactory.create()
+        self.offer = OfferFactory.create()
+        self.offer.publish()
         self.client.force_login(self.user)
 
     def test_user_join_offer_authenticated(self):
         """Test offer's created status for user."""
         post = self.client.get('/api/offers/{}/'.format(self.offer.id))
         self.assertEqual(post.data['joined'], False)
-        response = self.client.post(reverse('offer-join', kwargs={'pk': '1'}))
+        response = self.client.post(reverse(
+            'offer-join', kwargs={'pk': self.offer.id})
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         post = self.client.get('/api/offers/1/')
         self.assertEqual(post.data['joined'], True)
