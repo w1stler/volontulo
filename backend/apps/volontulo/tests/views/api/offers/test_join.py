81e0b67d51f2dda1df73c69fc17cf1c6ca3973bd
w1stler <wojciech.patelka@gmail.com>
2018-10-03 19:59:00 +0200

    [981] Fixes after CR

--- backend/apps/volontulo/tests/views/api/offers/test_join.py
+++ backend/apps/volontulo/tests/views/api/offers/test_join.py
@@ -3,27 +3,45 @@
 """
 .. module:: test_join
 """
+from django.urls import reverse
 
 from rest_framework import status
 from rest_framework.test import APITestCase
 
 from apps.volontulo.tests.views.offers.commons import TestOffersCommons
 
+from apps.volontulo.factories import UserFactory
+from apps.volontulo.factories import OfferFactory
+
 
 class TestAuthenticatedUserJoinOffer(TestOffersCommons, APITestCase):
 
     """Tests for REST API's join offer view for authenticated user."""
+    @classmethod
+    def setUpClass(cls):
+        cls.user = UserFactory.create()
+        cls.offer = OfferFactory.create()
+        cls.offer.publish()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.user.delete()
+        cls.offer.organization.delete()
+        cls.offer.delete()
 
     def setUp(self):
         """Set up each test."""
         super(TestAuthenticatedUserJoinOffer, self).setUp()
-        self.client.login(username='admin@example.com', password='123admin')
+        self.client.force_login(self.user)
 
     def test_user_join_offer_authenticated(self):
-        """Test offer's created status for admin user."""
-        response = self.client.post('/api/offers/1/join/')
-
+        """Test offer's created status for user."""
+        post = self.client.get('/api/offers/{}/'.format(self.offer.id))
+        self.assertEqual(post.data['joined'], False)
+        response = self.client.post(reverse('offer-join', kwargs={'pk': '1'}))
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        post = self.client.get('/api/offers/1/')
+        self.assertEqual(post.data['joined'], True)
 
     def test_user_join_not_existing_offer(self):
         """Test offer's 404 status for non existing offer."""
